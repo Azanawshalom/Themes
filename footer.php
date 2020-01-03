c7e8a7914e512b6c1ef83495cc7dcc921aaa0f96
Azanawshalom <39374482+Azanawshalom@users.noreply.github.com>
2020-01-03 14:11:42 +0300

    Add files via upload

--- footer.php
+++ footer.php
@@ -6,25 +6,46 @@
  *
  * @link https://developer.wordpress.org/themes/basics/template-files/#template-partials
  *
- * @package Nina
+ * @package Vibrant
  */
 
 ?>
 
 	</div><!-- #content -->
+	<nav id="site-navigation"  class="navbar navbar-expand-md navbar-dark top bg-dark">
+    <a class="navbar-brand" href="#">
+        <?php bloginfo('name'); ?>
+    </a>
+    <button class="menu-toggle" aria-controls="primary-menu" aria-expanded="false"><?php esc_html_e( 'Primary Menu', 'vibrant' ); ?></button>
+<?php
+            wp_nav_menu([
+            'menu'            => 'primary-menu',
+            'theme_location'  => 'menu-1',
+            'container'       => 'div',
+            'container_id'    => 'navbarCollapse',
+            'container_class' => 'collapse navbar-collapse',
+            'menu_id'         => false,
+            'menu_class'      => 'navbar-nav mr-auto',
+            'depth'           => 0,
+            'fallback_cb'     => 'functions::fallback',
+            'walker'          => new vibrant()
+            ]);
+        ?>
+        </nav>
+        </div>
 
 	<footer id="colophon" class="site-footer">
 		<div class="site-info">
-			<a href="<?php echo esc_url( __( 'https://wordpress.org/', 'nina' ) ); ?>">
+			<a href="<?php echo esc_url( __( 'https://wordpress.org/', 'vibrant' ) ); ?>">
 				<?php
 				/* translators: %s: CMS name, i.e. WordPress. */
-				printf( esc_html__( 'Proudly powered by %s', 'nina' ), 'WordPress' );
+				printf( esc_html__( 'Proudly powered by %s', 'vibrant' ), 'WordPress' );
 				?>
 			</a>
 			<span class="sep"> | </span>
 				<?php
 				/* translators: 1: Theme name, 2: Theme author. */
-				printf( esc_html__( 'Theme: %1$s by %2$s.', 'nina' ), 'nina', '<a href="http://underscores.me/">Underscores.me</a>' );
+				printf( esc_html__( 'Theme: %1$s by %2$s.', 'vibrant' ), 'vibrant', '<a href="http://underscores.me/">Underscores.me</a>' );
 				?>
 		</div><!-- .site-info -->
 	</footer><!-- #colophon -->
