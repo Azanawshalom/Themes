c5691f7b1e2f930cd2782166e112eb854957fa27
Azanawshalom <39374482+Azanawshalom@users.noreply.github.com>
2020-01-27 10:11:31 +0300

    Add files via upload

--- functions.php
+++ functions.php
@@ -158,13 +158,15 @@ add_action( 'widgets_init', 'vibrant_widgets_init' );
  * Enqueue scripts and styles.
  */
 function vibrant_scripts() {
-	wp_enqueue_style("s",get_template_directory_uri().'/assets/css/s.css',array(), '1.0','all');
+
+	wp_enqueue_style("s", get_template_directory_uri().'/assets/css/s.css',array(), '1.0','all');
 
 	wp_enqueue_style( 'vibrant-style', get_stylesheet_uri() );
 
 	wp_enqueue_script( 'vibrant-navigation', get_template_directory_uri() . '/js/navigation.js', array(), '20151215', true );
 
 	wp_enqueue_script( 'vibrant-skip-link-focus-fix', get_template_directory_uri() . '/js/skip-link-focus-fix.js', array(), '20151215', true );
+
 
 	if ( is_singular() && comments_open() && get_option( 'thread_comments' ) ) {
 		wp_enqueue_script( 'comment-reply' );
@@ -296,11 +298,14 @@ function RadTheme_enqueue_styles() {
 
     wp_enqueue_style( 'bootstrap', get_template_directory_uri() . '/assets/css/bootstrap.min.css' );
     wp_enqueue_style( 'core', get_template_directory_uri() . '/style.css' );
+    
   
   }
   add_action( 'wp_enqueue_scripts', 'RadTheme_enqueue_styles');
   
   function RadTheme_enqueue_scripts() {
     wp_enqueue_script( 'bootstrap', get_template_directory_uri() . '/assets/js/vendor/bootstrap.bundle.min.js', array( 'jquery' ) );
+
+
   }
   add_action( 'wp_enqueue_scripts', 'RadTheme_enqueue_scripts');
